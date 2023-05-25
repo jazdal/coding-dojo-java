2c9a3eef02cf1160683f80d7e12fe444141a8c8c
Jasper Dalawangbayan <jazdal.md@gmail.com>
2023-05-25 17:01:18 +0100

    Updated 'person' test folder

--- person/Person.java
+++ person/Person.java
@@ -1,13 +1,26 @@
 public class Person {
+    // MEMBER VARIABLES
     private int age;
     private String name;
-    public Person(int ageParam, String nameParam) {
-        this.age = ageParam;
-        this.name = nameParam;
+
+    // STATIC VARIABLE (BELONGS TO THE CLASS ITSELF, NOT THE INSTANCE OF THE CLASS)
+    private static int numberOfPeople = 0;
+
+    // CONSTRUCTOR
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        numberOfPeople++;
     }
+
     public void objectMethods(Person anotherObject) {
         System.out.println("Class name: " + this.getClass().getSimpleName());
         System.out.println("toString: " + this.toString());
         System.out.println("Equals: " + this.equals(anotherObject));
+    }
+
+    // STATIC METHOD (CLASS METHOD - CANNOT ACCESS INSTANCE VARIABLES ('THIS') AND METHODS DIRECTLY)
+    public static int peopleCount() {
+        return numberOfPeople;
     }
 }
--- person/PersonTest.java
+++ person/PersonTest.java
@@ -1,7 +1,9 @@
 public class PersonTest {
     public static void main(String[] args) {
-        Person person1 = new Person(10, "Person1");
-        Person person2 = new Person(5, "Person2");
-        person1.objectMethods(person2);
+        Person person1 = new Person("Tom", 33);
+        Person person2 = new Person("Jane", 23);
+        Person person3 = new Person("Sam", 29);
+        
+        System.out.println(Person.peopleCount());
     }
 }
