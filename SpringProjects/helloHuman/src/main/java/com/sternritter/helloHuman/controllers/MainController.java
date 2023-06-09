a72833fa94e6313e832524b6ed1771a6dcbaa720
Jasper Dalawangbayan <jazdal.md@gmail.com>
2023-06-09 14:28:23 +0800

    Added 'Hello Human' Spring Boot project

---
+++ SpringProjects/helloHuman/src/main/java/com/sternritter/helloHuman/controllers/MainController.java
@@ -0,0 +1,31 @@
+package com.sternritter.helloHuman.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+	@RequestMapping("/")
+	public String greeting(
+			@RequestParam(value="name", required=false) String name, 
+			@RequestParam(value="last_name", required=false) String last_name, 
+			@RequestParam(value="times", required=false) Integer times
+			) {
+		if (name == null) {
+			return "Hello Human";
+		} else if (times == null) {
+			if (last_name == null) {
+				return "Hello " + name;
+			} else {
+				return "Hello " + name + " " + last_name;
+			}
+		} else {
+			if (last_name == null) {
+				return ("Hello " + name + " ").repeat(times);
+			} else {
+				return ("Hello " + name + " " + last_name + " ").repeat(times);
+			}
+		}
+	}
+}
